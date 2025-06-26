272c5950006e82b7caa14d4e2a1307f204182fe8
SAMUEL FERNANDES DE LIMA <a95305356@rn.senac.local>
2025-06-26 08:35:05 -0300

    Exercicio 3 completo

---
+++ exercicio3.ts
@@ -0,0 +1,12 @@
+let numero : number = 6;
+let string : string = "olá";
+let falso : boolean = false;
+let indefinida : undefined;
+
+console.log(numero + string + falso + indefinida);
+console.log(numero * 2);
+if (falso === false){
+    console.log("è falso")
+}
+
+
