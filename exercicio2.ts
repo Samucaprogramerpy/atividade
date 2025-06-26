6894baddb8b7f4d33d6ace6dac788a08f2532547
SAMUEL FERNANDES DE LIMA <a95305356@rn.senac.local>
2025-06-26 08:30:25 -0300

    Exercicio 2 completo

---
+++ exercicio2.ts
@@ -0,0 +1,15 @@
+
+//Armazena um valor to tipo string e esse valor é o nome Samuel. O valor armazenado pode ser alterado quanto o tipo de dado armazenado//
+let name : string = "samuel";
+
+
+//Armazena um valor que pode ser acessado em todo o escopo do código, se não for armazenado dentro de uma função
+var numero : number = 6;
+
+
+//Armazena um valor fixo, que não pode ser alterado
+const CPF : number = 655.498.732-12;
+
+
+
+
